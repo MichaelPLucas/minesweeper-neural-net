ad9950003ef45d3f97d01d9990ef3569658ec040
Michael Lucas <lucasmichael221@gmail.com>
2019-11-22 22:56:33 -0500

    Initial commit. Progress so far is board generation and printing.

---
+++ board.py
@@ -0,0 +1,27 @@
+import random
+
+class Tile:
+  def __init__(self, x, y, is_bomb):
+    self.x = x
+    self.y = y
+    self.is_bomb = is_bomb
+
+class Board:
+  def __init__(self, width, height, num_bombs):
+    self.width = width
+    self.height = height
+    self.num_bombs = num_bombs
+    self.tiles = {}
+
+  def generate_new_board(self):
+    bombs = random.sample([(x, y) for y in range(self.height) for x in range(self.width)], k=self.num_bombs)
+    tiles = [Tile(x, y, (x, y) in bombs) for y in range(self.height) for x in range(self.width)]
+    for tile in tiles:
+      self.tiles[(tile.x, tile.y)] = tile
+
+  def print_board(self):
+    bomb_grid = [[0 for _ in range(self.width)] for _ in range(self.height)]
+    for tile in self.tiles.values():
+      bomb_grid[tile.y][tile.x] = 1 if tile.is_bomb else 0
+    for row in bomb_grid:
+      print(row)
